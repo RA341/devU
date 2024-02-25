df207a39d0afc1e9d0ce828f875e9d6bdd6f3fef
RA341 <razerishi1505@gmail.com>
2024-02-24 22:03:55 -0500

    added type for deadlineextension

---
+++ devu-shared/src/types/deadlineExtensions.ts
@@ -0,0 +1,9 @@
+export type DeadlineExtensions = {
+  id?: number
+  creatorId: number
+  userId: number
+  deadlineDate: string
+  assignmentId: number
+  createdAt?: string
+  updatedAt?: string
+}
