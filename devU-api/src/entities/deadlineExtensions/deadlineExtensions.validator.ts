d6e76400fde51fe916bb1c57e5704a57630532f3
RA341 <razerishi1505@gmail.com>
2024-02-29 22:47:35 -0500

    removed id equality

--- devU-api/src/entities/deadlineExtensions/deadlineExtensions.validator.ts
+++ devU-api/src/entities/deadlineExtensions/deadlineExtensions.validator.ts
@@ -1,8 +1,7 @@
 import { check } from 'express-validator'
-import { isSameId } from '../../middleware/validator/userId.validator'
 
 const creatorId = check('creatorId').isNumeric()
-const userId = check('userId').isNumeric().custom(isSameId('creatorId'))
+const userId = check('userId').isNumeric()
 const assignmentId = check('assignmentId').isNumeric()
 const deadlineDate = check('deadlineDate').trim().isISO8601().toDate()
 
