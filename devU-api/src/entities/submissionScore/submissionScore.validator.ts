d09233797c70ae6418a4b646c1c5acb1ad990aa0
RA341 <razerishi1505@gmail.com>
2024-04-12 12:57:24 -0400

    s

--- devU-api/src/entities/submissionScore/submissionScore.validator.ts
+++ devU-api/src/entities/submissionScore/submissionScore.validator.ts
@@ -5,7 +5,7 @@ import validate from '../../middleware/validator/generic.validator'
 const submissionId = check('submissionId').isNumeric()
 const score = check('score').isNumeric().optional({ nullable: true })
 const feedback = check('feedback').isString().trim().optional({ nullable: true })
-const releasedAt = check('releasedAt').trim().isISO8601().toDate()
+const releasedAt = check('releasedAt').trim().isISO8601().toDate().exists().withMessage("Make sure date is formatted correctly")
 
 const validator = [submissionId, score, feedback, releasedAt, validate]
 
--- devU-api/src/tango/tests/tango.endpoint.test.ts
+++ devU-api/src/tango/tests/tango.endpoint.test.ts
@@ -1,9 +1,76 @@
-import { getInfo, preallocateInstances } from '../tango.service'
+// ts errors here aare not relevant since
+// this should never run in the actual app.
+// script runs fine
+// @ts-ignore
+import path from 'path'
+// @ts-ignore
+import fs from 'fs'
 
-async function main(){
-  const res = await getInfo()
-  const pre = preallocateInstances()
-  console.log(res)
+import { addJob, openDirectory, pollJob, uploadFile } from '../tango.service'
+import './tango.types'
+
+// simulates the tango workflow
+async function main() {
+  const labName = 'testLab'
+
+  const graderTestFiles = ['test_files/autograde.tar', 'test_files/Makefile']
+  const outPutFileName = 'test_output.txt'
+
+  try {
+    // create a new lab
+    const openRes = await openDirectory(labName)
+
+    if (openRes == null) {
+      console.error('Open dir returned null!!!')
+      return
+    }
+
+    // uplaod grader files
+    for (const file of graderTestFiles) {
+      const fPath = path.join(__dirname, file)
+      fs.readFile(fPath, async (err: any, data: any) => {
+        if (err) {
+          console.log('Failed to read file')
+          console.log(err)
+          return
+        }
+        const res = await uploadFile(labName, data, path.basename(file))
+        console.log(res)
+      })
+    }
+
+    const jobInfo: AddJobRequest = {
+      image: 'autograding_image',
+      output_file: outPutFileName,
+      jobName: 'Test job',
+      files: graderTestFiles.map(value => {
+        const filename = path.basename(value)
+        const fileInfo: FileInfo = {
+          destFile: filename,
+          localFile: filename,
+        }
+        return fileInfo
+      }),
+      timeout: 100,
+    }
+
+    // @ts-ignore
+    const res = await addJob(labName, jobInfo)
+
+    while (true){
+      const res = await pollJob(labName, outPutFileName)
+
+      // check job status
+      if ((res as PollFailureResponse).statusId !== undefined){
+        console.log('Job is running')
+        console.log(res)
+      }
+
+    }
+  } catch (e) {
+    console.log('errored out')
+    console.log(e)
+  }
 }
 
 main().then(value => {
